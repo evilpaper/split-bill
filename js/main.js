c2b60042faa21b1d87c9944f8580be844945b308
pellburt@gmail.com <pellburt@gmail.com>
2019-02-09 10:35:13 +0100

    Tweaked split bill button, positioned the pen and applied toggle

--- js/main.js
+++ js/main.js
@@ -7,26 +7,27 @@ const displayBillAmount = app.querySelector(".sp__display--bill-amount");
 const displayNumberOfFriends = app.querySelector(".sp__display--friends");
 const displayTipAmount = app.querySelector(".sp__display--tip-amount");
 const displayTipPercentage = app.querySelector(".sp__display--tip-percentage");
-const sliderDisplayNumberOffFriends = app.querySelector(".sp__friends-number")
+const sliderDisplayNumberOffFriends = app.querySelector(".sp__friends-number");
 const splittedBillContainer = app.querySelector(".sp__splitted_bill-container");
 
 const display = document.querySelector(".sp__display");
-const sliderBlock = app.querySelector(".sp__friends-container")
+const sliderBlock = app.querySelector(".sp__friends-container");
 const friendsSlider = app.querySelector(".sp__friends-slider");
 const tipKeys = app.querySelector(".sp__tip-keys");
 const numberKeys = app.querySelector(".sp__number-keys");
 const splitBillKey = app.querySelector(".sp__split-button");
+const splitBillEditPen = app.querySelector(".sp__split-button-pen");
 
-const getKeyType = (key) => {
-  const { action } = key.dataset
-  if (!action) return 'number'
-  if (action === "clear") return 'clear'
-  return action
-}
+const getKeyType = key => {
+  const { action } = key.dataset;
+  if (!action) return "number";
+  if (action === "clear") return "clear";
+  return action;
+};
 
 const createBillString = (key, billAmount) => {
   const keyContent = key.textContent;
-  const keyType = getKeyType(key)
+  const keyType = getKeyType(key);
   const displayNumber = billAmount;
 
   if (keyType === "clear") {
@@ -43,20 +44,26 @@ const createBillString = (key, billAmount) => {
     if (displayNumber.length > 7) {
       return displayNumber;
     }
-      return displayNumber + keyContent;
+    return displayNumber + keyContent;
   }
 };
 
 const createTipsString = () => {
-  const bill = parseFloat(display.querySelector(".sp__display--bill-amount")
-    .textContent);
-  const tip = parseFloat(display.querySelector(".sp__display--tip-percentage")
-    .textContent
-    .split("(").join(" ")
-    .split("%)").join(" ")
-    );
-  return (bill * (tip / 100)) === Math.floor(bill * (tip / 100)) ? (bill * (tip / 100)) : (bill * (tip / 100)).toFixed(2);
-}
+  const bill = parseFloat(
+    display.querySelector(".sp__display--bill-amount").textContent
+  );
+  const tip = parseFloat(
+    display
+      .querySelector(".sp__display--tip-percentage")
+      .textContent.split("(")
+      .join(" ")
+      .split("%)")
+      .join(" ")
+  );
+  return bill * (tip / 100) === Math.floor(bill * (tip / 100))
+    ? bill * (tip / 100)
+    : (bill * (tip / 100)).toFixed(2);
+};
 
 const createTotalString = _ => {
   const bill = parseFloat(displayBillAmount.textContent);
@@ -68,8 +75,7 @@ const createTotalString = _ => {
 const highlightSelectedKey = key => {
   Array.from(key.parentNode.children).forEach(k => {
     k.classList.remove("is-selected");
-    }
-  );
+  });
   key.classList.add("is-selected");
 };
 
@@ -78,35 +84,35 @@ const updateFriendsNumbers = _ => {
   displayNumberOfFriends.textContent = friendsSlider.value;
   sliderDisplayNumberOffFriends.textContent = friendsSlider.value;
   sliderDisplayNumberOffFriends.classList.add("run-push-animation");
-  sliderDisplayNumberOffFriends.addEventListener("animationend", function() {sliderDisplayNumberOffFriends.classList.remove("run-push-animation");});
+  sliderDisplayNumberOffFriends.addEventListener("animationend", function() {
+    sliderDisplayNumberOffFriends.classList.remove("run-push-animation");
+  });
   updateFriendsIcons(friendsToBeAdded);
-}
+};
 
-const updateFriendsIcons = (friends) => {
-  const friendsContainer = app.querySelector(".sp__friends-icon-container")
+const updateFriendsIcons = friends => {
+  const friendsContainer = app.querySelector(".sp__friends-icon-container");
 
   const dynamicFriendsElements = app.querySelectorAll(".dynamic");
-  const friendsToBeRemoved = [...dynamicFriendsElements]
+  const friendsToBeRemoved = [...dynamicFriendsElements];
   friendsToBeRemoved.forEach(friend => {
-    friendsContainer.removeChild(friend)
-  })
+    friendsContainer.removeChild(friend);
+  });
 
-  const drawFriendsIcons = (times) => {
+  const drawFriendsIcons = times => {
     if (times === 0) return;
     setTimeout(() => {
       const friendElement = `<img class="sp__friends-icon-small dynamic run-small-push-animation" src="img/small_user.svg" alt="Friends icon">`;
-      friendsContainer.insertAdjacentHTML('beforeend', friendElement);
+      friendsContainer.insertAdjacentHTML("beforeend", friendElement);
       drawFriendsIcons(times - 1);
-    }
-    , 50)
-  }
+    }, 50);
+  };
 
   drawFriendsIcons(friends);
-
-}
+};
 
 const createSplittedBill = _ => {
-  const friends = [...Array(parseFloat(friendsSlider.value))]
+  const friends = [...Array(parseFloat(friendsSlider.value))];
 
   friends.forEach(friend => {
     const partOfBill = document.createElement("li");
@@ -115,16 +121,16 @@ const createSplittedBill = _ => {
       <h3 class="sp__parts_of_bill-person">Person A</h3>
       <h2 class="sp__parts_of_bill-amount">$10</h2>
       `;
-    splittedBillContainer.appendChild(partOfBill)
-  })
-}
+    splittedBillContainer.appendChild(partOfBill);
+  });
+};
 
 const removeSplittedBill = _ => {
   splittedBillContainer.remove();
-}
+};
 
 tipKeys.addEventListener("click", e => {
-  const targetButton = e.target.closest('button')
+  const targetButton = e.target.closest("button");
   if (targetButton) {
     const key = e.target.textContent;
     tip = parseFloat(key);
@@ -136,7 +142,7 @@ tipKeys.addEventListener("click", e => {
 });
 
 numberKeys.addEventListener("click", e => {
-  const key = e.target.closest("button")
+  const key = e.target.closest("button");
   if (key) {
     const billAmount = displayBillAmount.textContent;
     displayBillAmount.textContent = createBillString(key, billAmount);
@@ -144,30 +150,31 @@ numberKeys.addEventListener("click", e => {
     displayTotalAmount.textContent = createTotalString(billAmount);
     key.classList.add("run-push-animation");
   }
-  key.addEventListener("animationend", function() {key.classList.remove("run-push-animation");});
+  key.addEventListener("animationend", function() {
+    key.classList.remove("run-push-animation");
+  });
 });
 
 splitBillKey.addEventListener("click", e => {
-
-  if (displayTotalAmount.textContent === "$0") return
+  if (displayTotalAmount.textContent === "$0") return;
 
   if (sliderBlock.classList.contains("hidden")) {
-    removeSplittedBill()
+    removeSplittedBill();
   } else {
-    createSplittedBill()
+    createSplittedBill();
   }
 
-  splitBillKey.classList.toggle("shrinked")
+  splitBillKey.classList.toggle("shrinked");
+
   if (splitBillKey.classList.contains("shrinked")) {
     splitBillKey.innerHTML = "";
   } else {
     splitBillKey.innerHTML = "Split bill";
   }
 
-
   // Hide current content
   sliderBlock.classList.toggle("hidden");
   tipKeys.classList.toggle("hidden");
   numberKeys.classList.toggle("hidden");
-
-})
+  splitBillEditPen.classList.toggle("hidden");
+});
